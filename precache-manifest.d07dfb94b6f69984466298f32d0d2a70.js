e7766e1763f0dbbaf1a37c602422612c5b0e533b
Mariano <mm@mm.com>
2019-09-03 14:40:47 -0300

    react build

---
+++ precache-manifest.d07dfb94b6f69984466298f32d0d2a70.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d40520e73d002f8f79a2e6b14543b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2fdf07bd17a3f170c3e",
+    "url": "/static/js/2.f9272d28.chunk.js"
+  },
+  {
+    "revision": "4a7c91c0e8bdbefdb403",
+    "url": "/static/js/main.2fb1e9f3.chunk.js"
+  },
+  {
+    "revision": "42203eb4c16b5adc7026",
+    "url": "/static/js/runtime~main.abd80d82.js"
+  }
+]);
