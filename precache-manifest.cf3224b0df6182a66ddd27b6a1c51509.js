fa133c65e37284d097cf14c426d2164334943705
Mariano <mm@mm.com>
2019-09-04 16:18:57 -0300

    v

---
+++ precache-manifest.cf3224b0df6182a66ddd27b6a1c51509.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a7b37c3d2ec5f1f4f021d1d7649327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e153581fed7d326fa5d6",
+    "url": "/static/css/main.fed5f0a6.chunk.css"
+  },
+  {
+    "revision": "d1293315084d99bbb5f7",
+    "url": "/static/js/2.3a96db3b.chunk.js"
+  },
+  {
+    "revision": "e153581fed7d326fa5d6",
+    "url": "/static/js/main.fbfe2079.chunk.js"
+  },
+  {
+    "revision": "fb271c7869fdc969caf6",
+    "url": "/static/js/runtime~main.1499e8e2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0638261e8cf0ab931b7710d430b439c0.js"
+  "/precache-manifest.cf3224b0df6182a66ddd27b6a1c51509.js"
 );
 
 self.addEventListener('message', (event) => {
